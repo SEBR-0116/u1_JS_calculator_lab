32e4432004ab430898643f420e877f7d820f6c07
whoever11 <cjthesedays000@gmail.com>
2024-01-22 15:56:48 -0500

    final final

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ function calculate() {
             alert('try again')
         }
     }
+    function clearScreen() {
+        document.getElementById("result").value = ''
+    }
+
+    function display(value) {
+        document.getElementById("result").value += value
+    }
+
+    function calculate() {
+        const display = document.getElementById("result")
+        if (display.value) {
+            display.value = Function("return " + display.value)()
+            localStorage.setItem("result", display.value) // Store the result in localStorage
+        } else {
+            alert('try again')
+        }
+    }
