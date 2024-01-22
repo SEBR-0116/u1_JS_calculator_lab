eb7380242cc1564785e724b32cb59e7502881e6c
whoever11 <cjthesedays000@gmail.com>
2024-01-22 15:43:23 -0500

    new javascript

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
 function clearScreen() {
-    document.getElementById("result").value = ""
+    document.getElementById("result").value = ''
 }
 
 function display(value) {
     document.getElementById("result").value += value
 }
+
 function calculate() {
-    var p = document.getElementById("result").value
-    var q = eval(p)
-    document.getElementById("result").value = q
-}function add() {
-    var p = document.getElementById("result").value
-    var q = eval(p)
-    document.getElementById("result").value = q
-}
-
-function subtract() {
-    var p = document.getElementById("result").value
-    var q = eval(p)
-    document.getElementById("result").value = q
-}
-
-function multiply() {
-    var p = document.getElementById("result").value
-    var q = eval(p)
-    document.getElementById("result").value = q
-}
-
-function divide() {
-    var p = document.getElementById("result").value
-    var q = eval(p)
-    document.getElementById("result").value = q
-}
+        const display = document.getElementById("result")
+        if (display.value) {
+            display.value =Function("return " + display.value)()
+        } else {
+            alert('try again')
+        }
+    }
