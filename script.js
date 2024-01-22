76cb460a4b11d894c72ecd427e60927c2fe2f3e8
whoever11 <cjthesedays000@gmail.com>
2024-01-22 16:30:44 -0500

    done

--- script.js
+++ script.js
@@ -14,19 +14,3 @@ function calculate() {
             alert('try again')
         }
     }
-    function clearScreen() {
-        document.getElementById("result").value = ''
-    }
-
-    function display(value) {
-        document.getElementById("result").value += value
-    }
-
-    function calculate() {
-        const display = document.getElementById("result")
-        if (display.value) {
-            display.value = Function("return " + display.value)()
-        } else {
-            alert('try again')
-        }
-    }
