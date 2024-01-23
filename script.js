3ef94760f7f61157ee499789ba809d258df72197
AbeGue02 <abrahamgue02@gmail.com>
2024-01-22 19:58:29 -0500

    Adds better dot logic

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // GLOBAL VARIABLES
 let result = 0
 let previousResult = ""
+let storedOperation = ""
 let isSecondOperand = false
 
 const validCharacters = ['0','1','2','3','4','5','6','7','8','9','0','.']
@@ -11,36 +12,92 @@ const resultText = document.querySelector('#result')
 const previousResultText = document.querySelector('#previousOperation')
 const buttons = document.querySelectorAll('td')
 
+// Functions
+
+const resetCalculator = () => {
+    resultText.value = 0
+    previousResultText.innerHTML = 0
+    result = 0
+    previousResult = 0
+    isSecondOperand = false
+}
+
+const binaryOperation = (binaryOperator) => {
+    result = 0
+    storedOperation = `${previousResult} ${binaryOperator} ${result}`
+    isSecondOperand = true
+}
+
+const inputNumber = () => {
+    if ([...resultText.value].every(character => validCharacters.some((number) => number == character))) {
+        //If a valid character has been typed
+        previousResult = resultText.value
+    } else {
+        resultText.value = resultText.value.slice(0, -1)
+    }
+    
+    if (resultText.value[0] == "0" && resultText.value.length > 1 && resultText.value[1] != '.') {
+        resultText.value = resultText.value.slice(1)
+        if (resultText.value == '.') {
+            resultText.value = '0.'
+        }
+        previousResult = resultText.value
+    }
+
+    if (resultText.value.length === 0) {
+        resultText.value = "0"
+        previousResult = resultText.value
+    } 
+
+    result = resultText.value
+    previousResultText.innerHTML = previousResult
+}
 
 // Event Listeners
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         if (button.classList.contains('number')) {
-            resultText.value == "0" ? resultText.value = button.innerHTML : resultText.value += button.innerHTML
-            previousResultText.innerHTML == "0" ? previousResultText.innerHTML = button.innerHTML : previousResultText.innerHTML += button.innerHTML
+            if (!isSecondOperand) {
+                resultText.value += button.innerHTML
+                previousResultText.innerHTML += button.innerHTML
+                inputNumber()
+            }
         } else if (button.className == 'basicOperator' || button.className == 'advancedOperator') {
             switch (button.id) {
                 case 'ac':
-                    resultText.value = 0
-                    previousResultText.innerHTML = 0
+                    resetCalculator()
+                    break;
+                case '+':
+                    binaryOperation('+')
+                    
+                    break;
+
             }
         }
     })
 })
 
-resultText.addEventListener('input', (event) => {
-    if ([...event.target.value].every(character => validCharacters.some((number) => number == character))) {
+resultText.addEventListener('input', () => {
+    if ([...resultText.value].every(character => validCharacters.some((number) => number == character))) {
         //If a valid character has been typed
-        previousResult = event.target.value
+        previousResult = resultText.value
     } else {
-        event.target.value = event.target.value.slice(0, -1)
+        resultText.value = resultText.value.slice(0, -1)
     }
     
-    if (event.target.value.length === 0) {
-        event.target.value = "0"
-        previousResult = event.target.value
+    if (resultText.value[0] == "0" && resultText.value.length > 1 && resultText.value[1] != '.') {
+        resultText.value = resultText.value.slice(1)
+        if (resultText.value == '.') {
+            resultText.value = '0.'
+        }
+        previousResult = resultText.value
     }
 
-    result = parseFloat(event.target.value)
+    if (resultText.value.length === 0) {
+        resultText.value = "0"
+        previousResult = resultText.value
+    } 
+
+    result = resultText.value
     previousResultText.innerHTML = previousResult
 })
