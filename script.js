cd51ce718cad06392c58100815af6172a6f46402
connorreidy1 <connorjreidy@gmail.com>
2024-01-22 21:06:41 -0700

    Broken JS<>HTML fix, code reworked

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const statusDisplay = document.querySelector('.screentext')
-const sampleText = () => `12345`
+const screen = document.querySelector('.screen');
+const buttons = document.querySelectorAll('.btn');
+const clear = document.querySelector('.btn-clear');
+const equal = document.querySelector('.btn-equal');
 
-statusDisplay.innerHTML = sampleText()
-
-
-console.log(sampleText)
+//function for click on button and it appearing on the screen
+buttons.forEach(function(button) {
+    button.addEventListener('click', function(event) {
+        //finds the value in the 'data-num' attribute of the btn in html
+        let value = event.target.dataset.num;
+        //if/else for if the equal is clicked then show the value result
+        //if clear is clicked, screen is cleared
+        //else keep allowing other values onto the screen
+        if (value === 'Equal') {
+            let answer = eval(screen.value);
+            screen.value = answer;
+        } else if (value === 'Clear') {
+            screen.value = '';
+        } else {
+            screen.value += value;
+        }
+    });
+});
