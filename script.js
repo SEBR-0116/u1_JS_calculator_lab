bb98f78d5d78c59c826cef0b4633d7ab73dd6502
shubham20242 <stekawad@mtu.edu>
2024-01-26 07:23:11 -0500

    Progressing with logic

--- script.js
+++ script.js
@@ -18,8 +18,109 @@
 // const getButton_zero = document.getElementById("zero")
 // const getButton_dot = document.getElementById("dot")
 // const getButton_equalto = document.getElementById("equalto")
+const getOperatorButtons = document.querySelectorAll(`.operator`)
+const getNumberButtons = document.querySelectorAll(`.number`)
+const getDisplay = document.getElementById("Calculator-display")
+const getButton_ac = document.getElementById("ac")
+const getButton_plusminus = document.getElementById("plus-minus")
+console.log(getNumberButtons)
 
-const getButtons = document.querySelectorAll(`.pick`)
-console.log(getButtons)
-//
+//declaring variables
+let firstInput = []
+let secondInput = []
+let operator = []
+let indexNum = null
+let indexOperator = null
+let displayString = null
+let currentButton = null
+let previousButton = []
 
+//functions
+function gettingNumbers()
+{
+    if (operator.length == 0)
+        {
+            let number1 = getNumberButtons[indexNum].dataset.value
+            firstInput.push(number1)
+            displayString = firstInput.join('')
+            getDisplay.innerHTML = displayString
+            indexNum= null
+        }
+    else{
+            getDisplay.innerHTML = ``
+            let number2 = getNumberButtons[indexNum].dataset.value
+            secondInput.push(number2)
+            displayString = secondInput.join('')
+            getDisplay.innerHTML = displayString
+            indexNum= null
+        }       
+
+}
+
+function changeColor(button_in) {
+    currentButton = button_in
+    previousButton.push(currentButton)
+
+    if (previousButton.length==1) {
+      currentButton.style.backgroundColor = 'white'
+      currentButton.style.color = `rgb(205, 97, 15)`
+    }
+    else{
+        currentButton.style.backgroundColor = 'white'
+        currentButton.style.color = `rgb(205, 97, 15)`
+        previousButton[0].style.backgroundColor = `rgb(205, 97, 15)`
+        previousButton[0].style.color = 'white'
+        previousButton.shift()
+    }
+}
+
+function gettingoperators()
+{
+            let operator1 = getOperatorButtons[indexOperator].dataset.value
+            operator[0]=operator1     
+}
+
+
+
+//Adding event listners
+
+for (let i=0;i< getNumberButtons.length;i++)
+{
+    getNumberButtons[i].addEventListener('click',()=>{
+        indexNum = i
+        gettingNumbers()   
+    })
+}
+
+for (let j=0;j< getOperatorButtons.length;j++)
+{
+    getOperatorButtons[j].addEventListener('click',(event1) =>{
+
+        indexOperator = j
+        gettingoperators() 
+    }
+    )
+}
+
+getButton_ac.addEventListener('click',()=>{
+    getDisplay.innerHTML = `0` 
+})
+
+getButton_plusminus.addEventListener('click',()=>{
+    getDisplay.innerHTML =  ${`-`}
+})
+
+// switch(elementId){
+//     case plus:
+//         addNumbers()
+//     case minus:
+//         subtractNumbers()
+//     case multiply:
+//         multiplyNumbers()
+//     case divide:
+//         divideNumbers()
+//     case modulus:
+//         modulusOfNumbers()
+//     case ac:
+//         cleardisplay()
+// }
