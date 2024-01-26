f2143050065857bfdb5455780f90941849228322
andrew-taggart <andrew.taggart53@gmail.com>
2024-01-25 22:56:59 -0500

    button arguments; need to add solution

--- app.js
+++ app.js
@@ -1,44 +1,62 @@
-let inputDisplay = "0"
-let resultDisplay = false
+//Global
+const buttons = document.querySelectorAll('.button')
+const display = document.querySelector('.display')
+const clear = document.querySelector('#clear')
 
 
 //Functions
-function changeDisplay(value) {
-    if (inputDisplay = "0" || resultDisplay) {
-        inputDisplay = value
-    } else {
-        inputDisplay += value
-    }
-    resultDisplay = false
-    updateDisplay()
-}
-
-function updateDisplay() {
-    const display = document.getElementById("display")
-    display.textContent = inputDisplay
-}
-
-function calcResult() {
-    try {
-        const result = eval(inputDisplay)
-        inputDisplay += "\n=" + result.toString()
-        updateDisplay()
-    } catch (error) {
-    inputDisplay += "\nError"
-    updateDisplay()
-}
-
-resultDisplay = true
-}
-function cleaLastElement() {
-    currentDisplay = currentDisplay.slice(0,-1)
-    if (inputDisplay === "") {
-        inputDisplay = "0"
-    }
-    updateDisplay()
-}
 function clearDisplay() {
-    inputDisplay = "0"
-    resultDisplay = false
-    updateDisplay()
+    display.innerText = '0'
+} clearDisplay()
+//
+
+
+function getcurrentDisplay() {
+    let currentDisplay = display.innerHTML
+    currentDisplay = parseInt(currentDisplay)
+    return currentDisplay
 }
+
+function equals(event) {
+    document.querySelector("#solve").addEventListener('click', () => {
+        let 
+    })
+}
+
+//for Loop click Event
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', click =>  {
+        let input = buttons[i].innerText
+        if (display.innerHTML === '0' &&
+            buttons[i].id === 'operator') {
+            clearDisplay()
+        } else if (display.innerHTML === '0' && 
+                    buttons[i].id === 'clear'){
+                        clearDisplay()
+                    }
+            else if (display.innerHTML === '0' &&
+                    buttons[i].id === "btn") {
+                        display.innerText = input
+                    }
+         else if (buttons[i].id === "btn")
+        { 
+            display.innerText += input
+        } else if (buttons[i].id === "clear") {
+            clearDisplay()
+        } else if (buttons[i].id === "decimal") {
+            display.innerText += input
+            //Figure out how to Limit
+        } else if (buttons[i].id === "operator") {
+            let z = buttons[i].id.innerHTML
+            let x = display.innerText
+            clearDisplay()
+            console.log(z)
+            } else {
+                let y = display.innerHTML
+                equate = `${x}${z}${y}`
+                display.innerText = parseInt(equate)
+            }}
+          //else if (buttons[i].id === "solve"){
+
+                   
+        )}
